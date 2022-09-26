94607eb712670bf042e6f46c849ae0ef6342c80c
Alexandre <alexandrechanas0@gmail.com>
2022-09-26 19:17:29 +0200

    Co-authored-by: Alexandre Sieg <unkobweb@users.noreply.github.com>

--- sandwicherie_server/Ingredients.js
+++ sandwicherie_server/Ingredients.js
@@ -39,4 +39,144 @@ export const allIngredients = [
     type: "Viande",
     price: 1.2,
   },
+  {
+    name: "Bacon",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Cheddar",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Gruyère",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Jambon de Bayonne",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Jambon blanc",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Lardons",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Oignons",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Pâtes",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Salade",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Salami",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Saucisson",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "Tomate",
+    type: "Accompagnement",
+    price: 0.5,
+  },
+  {
+    name: "BBQ Miel",
+    type: "Sauce",
+    price: 0.2,
+  },
+  {
+    name: "Beurre",
+    type: "Sauce",
+    price: 0.2,
+  },
+  {
+    name: "Biggy",
+    type: "Sauce",
+    price: 0.2,
+  },
+  {
+    name: "Curry",
+    type: "Sauce",
+    price: 0.2,
+  },
+  {
+    name: "Ketchup",
+    type: "Sauce",
+    price: 0.2,
+  },
+  {
+    name: "Mayonnaise",
+    type: "Sauce",
+    price: 0.2,
+  },
+  {
+    name: "7up",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Coca-Cola",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Fanta",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Ice-tea",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Oasis",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Orangina",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Pepsi",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Sprite",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Tropico",
+    type: "Boisson",
+    price: 1.8,
+  },
+  {
+    name: "Cookie",
+    type: "Dessert",
+    price: 0.8,
+  },
 ];
