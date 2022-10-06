e013f9f4e6379c917b2a6f37a40d5376295f77c2
Alexandre <alexandre0chanas@gmail.com>
2022-10-06 13:13:32 +0200

    switch computer

--- sandwicherie/src/component/restaurant/RestaurantCommand.js
+++ sandwicherie/src/component/restaurant/RestaurantCommand.js
@@ -5,48 +5,39 @@ import { TiDelete } from "react-icons/ti";
 
 const RestaurantCommand = () => {
   const [user, setUser] = useState([]);
-  const [phone, setPhone] = useState("");
-  const [command, setCommand] = useState([]);
+  const [phone, setPhone] = useState([]);
+  const [command, setCommand] = useState({});
 
   const handleClick = async () => {
     await axios.delete("http://localhost:8000/delete", { data: user });
   };
 
+  let newArr = [];
+
   useEffect(() => {
     async function fetchData() {
       await axios.get("http://localhost:8000/command").then((res) => {
-        const users = res.data.data;
-        let arrUsers = [];
-        users.map((element) => {
-          setUser((state) => [...state, element]);
+        res.data.data.map((phone) => {
+          setPhone((oldPhone) => [...oldPhone, phone.phone]);
+          phone.commandes.map((element) => {
+            let arr = [];
+            element.ingredientsCommandes.map((ingredient) => {
+              let name = ingredient.ingredient.name;
+              let type = ingredient.ingredient.type;
+              arr.push([type, name]);
+            });
+            newArr.push(arr);
+          });
         });
       });
     }
     fetchData();
   }, []);
 
-  useEffect(() => {
-    let arr = [];
-
-    user.map((phone) => {
-      phone.commandes.map((commande) => {
-        commande.ingredientsCommandes.map((ingredient) => {
-          arr.push(ingredient.ingredient);
-        });
-      });
-    });
-  }, [user]);
-
-  console.log(command);
-
+  console.log(newArr);
   return (
     <div className="wrapper__restaurant">
-      {/* {phone.map((element) => {
-        <div className="element__restaurant">
-          <p>Telephone</p>
-        </div>;
-      })} */}
-
+      <div></div>
       <div className="command__restaurant">
         {/* {command.map((element) => (
           <div className="element__restaurant">
