64db1ba10fd960da894e06e44bdd09aa944f99e0
Alexandre <alexandrechanas0@gmail.com>
2022-10-10 07:08:15 +0200

    add delete on back

--- sandwicherie/src/component/restaurant/RestaurantCommand.js
+++ sandwicherie/src/component/restaurant/RestaurantCommand.js
@@ -4,42 +4,47 @@ import "../../styles/Restaurant/restaurant.css";
 import { TiDelete } from "react-icons/ti";
 
 const RestaurantCommand = () => {
-  const [user, setUser] = useState([]);
-  const [phone, setPhone] = useState([]);
   const [command, setCommand] = useState([]);
+  const [data, setData] = useState(null);
 
-  const handleClick = async () => {
-    await axios.delete("http://localhost:8000/delete", { data: user });
+  const handleDelete = (e) => {
+    let parent = e.currentTarget.parentNode.parentNode.className;
+    let id = parent.split(" ").slice(-1).join("");
+    axios.delete(`http://localhost:8000/command/${id}`);
   };
-  useEffect(() => {
-    async function fetchData() {
-      await axios.get("http://localhost:8000/command").then((res) => {
-        res.data.data.map((phone) => {
-          setPhone((oldPhone) => [...oldPhone, phone.phone]);
-          phone.commandes.map((element) => {
-            let arr = [];
-            element.ingredientsCommandes.map((ingredient) => {
-              let name = ingredient.ingredient.name;
-              let type = ingredient.ingredient.type;
-              arr.push([type, name]);
-            });
-            setCommand((oldCommand) => [...oldCommand, arr]);
-          });
-        });
+
+  const fetchData = async () => {
+    await axios.get("http://localhost:8000/command").then((res) => {
+      let command = res.data.data;
+      command.forEach((element) => {
+        setCommand((prevCommand) => [...prevCommand, element]);
       });
-    }
+    });
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
 
-  console.log(command);
-
   return (
     <div className="wrapper__restaurant">
-      <div className="command__restaurant">
-        {command.map((element) => (
-          <div>{element}</div>
-        ))}
-      </div>
+      {command.map((element, i) => (
+        <div className={`command__restaurant ${element.id}`}>
+          <div key={element.phone.id}>
+            <p>{element.phone}</p>
+          </div>
+          <div>
+            {element.commandes.map((element) =>
+              element.ingredientsCommandes.map((element) => (
+                <p key={element.ingredient.id}>{element.ingredient.name}</p>
+              ))
+            )}
+          </div>
+          <p className="delete__restaurant">
+            <TiDelete size={35} onClick={handleDelete} />
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
